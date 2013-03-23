b6678c2e7a327551ffaff9aac6ba2e7635752e2b
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-03-23 15:53:12 +0100

    Fixed typo in FindSSVUtils, added more PATHS

--- cmake/Modules/FindSSVUtils.cmake
+++ cmake/Modules/FindSSVUtils.cmake
@@ -9,23 +9,18 @@
 # also defined, but not for general use are
 # SSVUTILS_LIBRARY, where to find the SSVUtils library.
 
-
 FIND_PATH(SSVUTILS_INCLUDE_DIR 
   NAMES SSVUtils/SSVUtils.h
-  PATH_SUFFIXES include
+  PATH_SUFFIXES include/
   PATHS "${PROJECT_SOURCE_DIR}/extlibs/SSVUtils/"
   ${SSVUTILS_ROOT}
   $ENV{SSVUTILS_ROOT}
-  ~/Library/Frameworks
-  /Library/Frameworks
   /usr/local/
   /usr/
   /sw/         # Fink
   /opt/local/  # DarwinPorts
   /opt/csw/    # Blastwave
   /opt/
-  /lib
-  /lib64
 )
 
 message("\nFound SSVUtils include at: ${SSVUTILS_INCLUDE_DIR}.\n")
@@ -33,23 +28,29 @@ message("\nFound SSVUtils include at: ${SSVUTILS_INCLUDE_DIR}.\n")
 FIND_LIBRARY(SSVUTILS_LIBRARY
   NAMES SSVUtils libSSVUtils SSVUtils-s libSSVUtils-s ssvutils libssvutils ssvutils-s libssvutils-s
   PATH_SUFFIXES lib/ lib64/
-  PATHS /usr/ /usr/local/
+  PATHS "${PROJECT_SOURCE_DIR}/extlibs/SSVUtils/"
+  ${SSVUTILS_ROOT}
+  $ENV{SSVUTILS_ROOT}
+  /usr/local/
+  /usr/
+  /sw/         # Fink
+  /opt/local/  # DarwinPorts
+  /opt/csw/    # Blastwave
+  /opt/
 )
 
 message("\nFound SSVUtils library at: ${SSVUTILS_LIBRARY}.\n")
 
 IF (SSVUTILS_LIBRARY AND SSVUTILS_INCLUDE_DIR)
     SET(SSVUTILS_LIBRARIES ${SSVUTILS_LIBRARY})
-    SET(SSV_UTILS_FOUND TRUE)
+    SET(SSVUTILS_FOUND TRUE)
 ELSE (SSVUTILS_LIBRARY AND SSVUTILS_INCLUDE_DIR)
     SET(SSVUTILS_FOUND FALSE)
 ENDIF (SSVUTILS_LIBRARY AND SSVUTILS_INCLUDE_DIR)
 
 
 IF (SSVUTILS_FOUND)
-   #IF (NOT SSVUTILS_FIND_QUIETLY)
       MESSAGE(STATUS "Found SSVUTILS: ${SSVUTILS_LIBRARIES}")
-   #ENDIF (NOT SSVUTILS_FIND_QUIETLY)
 ELSE (SSVUTILS_FOUND)
    IF (SSVUTILS_FIND_REQUIRED)
       MESSAGE(FATAL_ERROR "Could not find SSVUTILS library")
