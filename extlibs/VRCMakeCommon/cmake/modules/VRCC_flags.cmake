57cf63a4c95206386a45e2bbf4725564c3935a47
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-05-31 17:54:30 +0200

    fixing submodules

--- extlibs/VRCMakeCommon/cmake/modules/VRCC_flags.cmake
+++
@@ -1,3 +0,0 @@
-set(CMAKE_CXX_FLAGS "-std=c++11 -Wall -Wextra -W -pedantic" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O3" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS_DEBUG "-fno-omit-frame-pointer -g3 -gdwarf-2 -O0" CACHE STRING "" FORCE)
