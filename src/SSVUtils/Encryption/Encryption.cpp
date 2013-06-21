7994fbbea674c7671f71bacca9d89e39d3bee6bb
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-21 16:29:32 +0200

    license updated

--- src/SSVUtils/Encryption/Encryption.cpp
+++ src/SSVUtils/Encryption/Encryption.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2013 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
 
 #include "SSVUtils/Encryption/Encryption.h"
 #include "SSVUtils/Encryption/Internal/Base64.h"
