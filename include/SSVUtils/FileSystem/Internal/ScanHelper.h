8e088cdccb0bf8ecf000b6f77585d197d178f6d3
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-04 16:42:52 +0200

    (formatting) Reduce nesting

--- include/SSVUtils/FileSystem/Internal/ScanHelper.h
+++ include/SSVUtils/FileSystem/Internal/ScanHelper.h
@@ -5,6 +5,8 @@
 #ifndef SSVU_FILESYSTEM_INTERNAL_SCANHELPER
 #define SSVU_FILESYSTEM_INTERNAL_SCANHELPER
 
+#include <dirent.h>
+#include <sys/stat.h>
 #include "SSVUtils/FileSystem/Enums.h"
 #include "SSVUtils/FileSystem/Utils.h"
 
@@ -16,35 +18,32 @@ namespace ssvu
 		{
 			template<Mode TM, Type TT, Pick TP, Sort TS> static void scan(std::vector<std::string>& mTarget, const std::string& mPath, const std::string& mDesired)
 			{
-				if(isFolder(mPath))
+				if(!isFolder(mPath)) lo << lt("ssvu::FileSystem::ScanHelper") << "Directory \"" << mPath << "\" not found" << std::endl;
+
+				DIR* dir{opendir(mPath.c_str())};
+				dirent* entry{readdir(dir)};
+
+				while(entry != NULL)
 				{
-					DIR* dir{opendir(mPath.c_str())};
-					dirent* entry{readdir(dir)};
-
-					while(entry != NULL)
+					std::string name{entry->d_name}, path{getNormalizedPath(getNormalizedPath(mPath) + name)};
+					if(!isRootOrParent(path))
 					{
-						std::string name{entry->d_name}, path{getNormalizedPath(getNormalizedPath(mPath) + name)};
-						if(!isRootOrParent(path))
+						if(isFolder(path))
 						{
-							if(isFolder(path))
-							{
-								if(TT == Type::All || TT == Type::Folder) { mTarget.push_back(path); }
-								if(TM == Mode::Recurse) { scan<Mode::Recurse, TT, TP, TS>(mTarget, path, mDesired); }
-							}
-							else if(TT == Type::All || TT == Type::File)
-							{
-								if(TP == Pick::Any)			{ mTarget.push_back(path); }
-								else if(TP == Pick::ByExt)	{ if(endsWith(name, mDesired)) mTarget.push_back(path); }
-								else if(TP == Pick::ByName)	{ if(name == mDesired) mTarget.push_back(path); }
-							}
+							if(TT == Type::All || TT == Type::Folder) { mTarget.push_back(path); }
+							if(TM == Mode::Recurse) { scan<Mode::Recurse, TT, TP, TS>(mTarget, path, mDesired); }
 						}
-
-						entry = readdir(dir);
+						else if(TT == Type::All || TT == Type::File)
+						{
+							if(TP == Pick::Any)			{ mTarget.push_back(path); }
+							else if(TP == Pick::ByExt)	{ if(endsWith(name, mDesired)) mTarget.push_back(path); }
+							else if(TP == Pick::ByName)	{ if(name == mDesired) mTarget.push_back(path); }
+						}
 					}
 
-					closedir(dir);
+					entry = readdir(dir);
 				}
-				else lo << lt("ssvu::FileSystem::ScanHelper") << "Directory \"" << mPath << "\" not found" << std::endl;
+				closedir(dir);
 
 				if(TS == Sort::Alphabetic) std::sort(std::begin(mTarget), std::end(mTarget));
 			}
