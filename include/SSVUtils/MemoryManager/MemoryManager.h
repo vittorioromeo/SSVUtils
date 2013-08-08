d53c2bb08d68d00baff5b2ce8fe3e8db3d77f0b5
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-08 02:49:12 +0200

    MemoryManager now uses CRTP for static polymorphism (dear god)

--- include/SSVUtils/MemoryManager/MemoryManager.h
+++ include/SSVUtils/MemoryManager/MemoryManager.h
@@ -11,54 +11,75 @@
 
 namespace ssvu
 {
-	template<typename T> class MemoryManager;
+	template<typename TDerived, typename T, typename TDeleter = std::default_delete<T>> class MemoryManagerBase;
 
 	class MemoryManageable
 	{
-		template<typename T> friend class MemoryManager;
+		template<typename TDerived, typename T, typename TDeleter> friend class MemoryManagerBase;
 
-		private:
+		public:
 			bool alive{true};
 
 		public:
 			inline bool isAlive() const	{ return alive; }
 	};
 
-	template<typename T> class MemoryManager
+	template<typename TDerived, typename T, typename TDeleter> class MemoryManagerBase
 	{
 		public:
-			using Container = std::vector<Uptr<T>>;
+			using TUptr = Uptr<T, TDeleter>;
+			using Container = std::vector<TUptr>;
 
-		private:
+		protected:
+			using Iterator = typename Container::iterator;
+			using ConstIterator = typename Container::const_iterator;
+
 			Container items;
 			std::vector<T*> toAdd;
-			struct Deleter { inline bool operator()(const Uptr<T>& mItem) const { return !mItem->alive; } } deleter;
+			struct Deleter { inline bool operator()(const TUptr& mItem) const { return !mItem->alive; } } deleter;
 
-		public:
-			inline void refresh()
-			{
-				items.erase(std::remove_if(std::begin(items), std::end(items), deleter), std::end(items));
-				for(const auto& i : toAdd) items.push_back(Uptr<T>(i)); toAdd.clear();
-			}
+		 public:
+			inline void refresh() { static_cast<TDerived*>(this)->refreshImpl(); }
 			inline void clear()	{ items.clear(); toAdd.clear(); }
 			inline void del(T& mItem) { mItem.alive = false; }
 
-			template<typename TType, typename... TArgs> inline TType& create(TArgs&&... mArgs) { TType* result{new TType(std::forward<TArgs>(mArgs)...)}; toAdd.push_back(result); return *result; }
-			template<typename... TArgs> inline T& create(TArgs&&... mArgs)	{ return create<T, TArgs...>(std::forward<TArgs>(mArgs)...); }
+			template<typename TType, typename... TArgs> inline TType& create(TArgs&&... mArgs)
+			{
+				return static_cast<TDerived*>(this)->template createTImpl<TType, TArgs...>(std::forward<TArgs>(mArgs)...);
+			}
+			template<typename... TArgs> inline T& create(TArgs&&... mArgs)
+			{
+				return static_cast<TDerived*>(this)->template createImpl<TArgs...>(std::forward<TArgs>(mArgs)...);
+			}
 
-			inline Container& getItems() { return items; }
-			inline const Container& getItems() const { return items; }
+			inline Container& getItems()				{ return items; }
+			inline const Container& getItems() const	{ return items; }
 
 			// Foreach loop/algorithms iterator support
-			using iterator = typename Container::iterator;
-			using const_iterator = typename Container::const_iterator;
+			Iterator begin()				{ return items.begin(); }
+			ConstIterator begin() const		{ return items.begin(); }
+			const Iterator cbegin() const	{ return items.cbegin(); }
+			Iterator end()					{ return items.end(); }
+			ConstIterator end() const		{ return items.end(); }
+			const Iterator cend() const		{ return items.cend(); }
+	 };
 
-			iterator begin()				{ return items.begin(); }
-			const_iterator begin() const	{ return items.begin(); }
-			const iterator cbegin() const	{ return items.cbegin(); }
-			iterator end()					{ return items.end(); }
-			const_iterator end() const		{ return items.end(); }
-			const iterator cend() const		{ return items.cend(); }
+	template<typename T, typename TDeleter = std::default_delete<T>> class MemoryManager : public MemoryManagerBase<MemoryManager<T, TDeleter>, T, TDeleter>
+	{
+		public:
+			inline void refreshImpl()
+			{
+				this->items.erase(std::remove_if(std::begin(this->items), std::end(this->items), this->deleter), std::end(this->items));
+				for(const auto& i : this->toAdd) this->items.emplace_back(i); this->toAdd.clear();
+			}
+			template<typename TType, typename... TArgs> inline TType& createTImpl(TArgs&&... mArgs)
+			{
+				auto result(new TType(std::forward<TArgs>(mArgs)...)); this->toAdd.push_back(result); return *result;
+			}
+			template<typename... TArgs> inline T& createImpl(TArgs&&... mArgs)
+			{
+				return createTImpl<T, TArgs...>(std::forward<TArgs>(mArgs)...);
+			}
 	};
 }
 
