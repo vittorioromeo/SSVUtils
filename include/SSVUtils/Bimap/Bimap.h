25d4969c259e74d5a4ecec72974046794090260b
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-14 14:37:37 +0200

    Formatting

--- include/SSVUtils/Bimap/Bimap.h
+++ include/SSVUtils/Bimap/Bimap.h
@@ -20,12 +20,12 @@ namespace ssvu
 			Bimap(const std::initializer_list<std::pair<T, U>>& mPairs) { for(const auto& p : mPairs) insert(p); }
 
 			inline void insert(const std::pair<T, U>& mPair)	{ map1[mPair.first] = mPair.second; map2[mPair.second] = mPair.first; }
+			inline void erase(const T& mValue)					{ map2.erase(map1.at(mValue)); map1.erase(mValue); }
+			inline void erase(const U& mValue)					{ map1.erase(map2.at(mValue)); map2.erase(mValue); }
 			inline const U& at(const T& mValue) const			{ return map1.at(mValue); }
 			inline const T& at(const U& mValue) const			{ return map2.at(mValue); }
 			inline bool has(const T& mValue) const				{ return map1.find(mValue) != end(map1); }
 			inline bool has(const U& mValue) const				{ return map2.find(mValue) != end(map2); }
-			inline void erase(const T& mValue)					{ map2.erase(map1.at(mValue)); map1.erase(mValue); }
-			inline void erase(const U& mValue)					{ map1.erase(map2.at(mValue)); map2.erase(mValue); }
 			inline decltype(map1)& getMap1()					{ return map1; }
 			inline decltype(map2)& getMap2()					{ return map2; }
 			inline const decltype(map1)& getMap1() const		{ return map1; }
