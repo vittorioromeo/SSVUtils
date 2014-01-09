0b60ecd9900521d6ba5378ef222be437c73acf35
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-01-09 12:48:11 +0100

    New ssvpp_tpl_get (no warnings?

--- include/SSVUtils/Preprocessor/Tuple.hpp
+++ include/SSVUtils/Preprocessor/Tuple.hpp
@@ -10,30 +10,56 @@
 
 namespace ssvu
 {
-	#define SSVPP_IMPL_TPL_GET_0(m0, ...) m0
-	#define SSVPP_IMPL_TPL_GET_1(m0, m1, ...) m1
-	#define SSVPP_IMPL_TPL_GET_2(m0, m1, m2, ...) m2
-	#define SSVPP_IMPL_TPL_GET_3(m0, m1, m2, m3, ...) m3
-	#define SSVPP_IMPL_TPL_GET_4(m0, m1, m2, m3, m4, ...) m4
-	#define SSVPP_IMPL_TPL_GET_5(m0, m1, m2, m3, m4, m5, ...) m5
-	#define SSVPP_IMPL_TPL_GET_6(m0, m1, m2, m3, m4, m5, m6, ...) m6
-	#define SSVPP_IMPL_TPL_GET_7(m0, m1, m2, m3, m4, m5, m6, m7, ...) m7
-	#define SSVPP_IMPL_TPL_GET_8(m0, m1, m2, m3, m4, m5, m6, m7, m8, ...) m8
-	#define SSVPP_IMPL_TPL_GET_9(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, ...) m9
-	#define SSVPP_IMPL_TPL_GET_10(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, ...) m10
-	#define SSVPP_IMPL_TPL_GET_11(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, ...) m11
-	#define SSVPP_IMPL_TPL_GET_12(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, ...) m12
-	#define SSVPP_IMPL_TPL_GET_13(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, ...) m13
-	#define SSVPP_IMPL_TPL_GET_14(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, ...) m14
-	#define SSVPP_IMPL_TPL_GET_15(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, ...) m15
-	#define SSVPP_IMPL_TPL_GET_16(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, ...) m16
-	#define SSVPP_IMPL_TPL_GET_17(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, ...) m17
-	#define SSVPP_IMPL_TPL_GET_18(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, ...) m18
-	#define SSVPP_IMPL_TPL_GET_19(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, ...) m19
-	#define SSVPP_IMPL_TPL_GET_20(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, ...) m20
+	#define SSVPP_TPL_MAX_SIZE 20
 
-	#define SSVPP_IMPL_TPL_GET(mIdx, mTpl)	SSVPP_TKNCAT_2(SSVPP_IMPL_TPL_GET_, mIdx) mTpl
-	#define SSVPP_TPL_GET(mIdx, mTpl)		SSVPP_IMPL_TPL_GET(mIdx, mTpl)
+	#define SSVPP_IMPL_TPL_FILL_20(m0) SSVPP_TPL_EXPLODE(m0)
+	#define SSVPP_IMPL_TPL_FILL_19(m0) SSVPP_TPL_EXPLODE(m0),		0
+	#define SSVPP_IMPL_TPL_FILL_18(m0) SSVPP_TPL_EXPLODE(m0),		0, 0
+	#define SSVPP_IMPL_TPL_FILL_17(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_16(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0 ,0
+	#define SSVPP_IMPL_TPL_FILL_15(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_14(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_13(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_12(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_11(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_10(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_9(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_8(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_7(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_6(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_5(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_4(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_3(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_2(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_1(m0) SSVPP_TPL_EXPLODE(m0),		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	#define SSVPP_IMPL_TPL_FILL_0(m0)								0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+	#define SSVPP_IMPL_TPL_GET_0(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m0
+	#define SSVPP_IMPL_TPL_GET_1(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m1
+	#define SSVPP_IMPL_TPL_GET_2(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m2
+	#define SSVPP_IMPL_TPL_GET_3(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m3
+	#define SSVPP_IMPL_TPL_GET_4(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m4
+	#define SSVPP_IMPL_TPL_GET_5(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m5
+	#define SSVPP_IMPL_TPL_GET_6(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m6
+	#define SSVPP_IMPL_TPL_GET_7(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m7
+	#define SSVPP_IMPL_TPL_GET_8(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m8
+	#define SSVPP_IMPL_TPL_GET_9(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m9
+	#define SSVPP_IMPL_TPL_GET_10(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m10
+	#define SSVPP_IMPL_TPL_GET_11(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m11
+	#define SSVPP_IMPL_TPL_GET_12(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m12
+	#define SSVPP_IMPL_TPL_GET_13(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m13
+	#define SSVPP_IMPL_TPL_GET_14(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m14
+	#define SSVPP_IMPL_TPL_GET_15(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m15
+	#define SSVPP_IMPL_TPL_GET_16(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m16
+	#define SSVPP_IMPL_TPL_GET_17(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m17
+	#define SSVPP_IMPL_TPL_GET_18(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m18
+	#define SSVPP_IMPL_TPL_GET_19(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m19
+	#define SSVPP_IMPL_TPL_GET_20(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19) m20
+
+	#define SSVPP_TPL_FILL_IMPL(mTpl)		SSVPP_TKNCAT_2(SSVPP_IMPL_TPL_FILL_, SSVPP_TPL_SIZE(mTpl)) (mTpl)
+	#define SSVPP_TPL_FILL(mTpl)			( SSVPP_TPL_FILL_IMPL(mTpl) )
+	#define SSVPP_IMPL_TPL_GET(mIdx, mTpl)	SSVPP_TKNCAT_2(SSVPP_IMPL_TPL_GET_, mIdx) SSVPP_EXPAND(SSVPP_TPL_FILL(mTpl))
+	#define SSVPP_TPL_GET(mIdx, mTpl)		SSVPP_EXPAND(SSVPP_IMPL_TPL_GET(mIdx, mTpl))
 
 	#define SSVPP_IMPL_TPL_MAKE(...)		(__VA_ARGS__)
 	#define SSVPP_TPL_MAKE(...)				SSVPP_IMPL_TPL_MAKE(__VA_ARGS__)
