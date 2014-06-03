d3b291c4e9b86cd2b53e3fefd6b36fa9eedc07b8
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-06-03 15:00:50 +0200

    test

--- include/SSVUtils/MemoryManager/MemoryManager.hpp
+++ include/SSVUtils/MemoryManager/MemoryManager.hpp
@@ -62,124 +62,6 @@ namespace ssvu
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 
-#ifndef SSVU_MEMORYMANAGER2
-#define SSVU_MEMORYMANAGER2
-
-#include "SSVUtils/Core/Core.hpp"
-
-namespace ssvu
-{
-	template<typename, typename> class MemoryManager2;
-
-	namespace Internal
-	{
-		template<typename T, typename TDeleter, typename TItrValue, typename TDerived> class MRItrBase
-		{
-			public:
-				using TUptr = Uptr<T, TDeleter>;
-
-			protected:
-				TItrValue value;
-
-				inline TDerived& getThisDerived() noexcept { return *reinterpret_cast<TDerived*>(this); }
-				template<typename TT> inline TT getImpl() noexcept { return getThisDerived().template getImpl<TT>(); }
-
-			public:
-				inline MRItrBase(TItrValue mValue) noexcept : value{mValue} { }
-
-				inline TDerived& operator++() noexcept				{ ++value;			return getThisDerived(); }
-				inline TDerived& operator++(int) noexcept			{ ++value;			return getThisDerived(); }
-				inline TDerived& operator--() noexcept				{ --value;			return getThisDerived(); }
-				inline TDerived& operator--(int) noexcept			{ --value;			return getThisDerived(); }
-				inline TDerived& operator+=(int mOffset) noexcept	{ value += mOffset;	return getThisDerived(); }
-				inline TDerived& operator-=(int mOffset) noexcept	{ value -= mOffset;	return getThisDerived(); }
-
-				inline TUptr& operator*() noexcept				{ return getImpl<TUptr&>(); }
-				inline const TUptr& operator*() const noexcept	{ return getImpl<const TUptr&>(); }
-				inline TUptr* operator->() noexcept				{ return &(getImpl<TUptr&>()); }
-				inline const TUptr* operator->() const noexcept	{ return &(getImpl<const TUptr&>()); }
-
-				inline bool operator==(const TDerived& mRhs) const noexcept	{ return value == mRhs.value; }
-				inline bool operator!=(const TDerived& mRhs) const noexcept	{ return value != mRhs.value; }
-				inline bool operator<(const TDerived& mRhs) const noexcept	{ return value < mRhs.value; }
-				inline bool operator>(const TDerived& mRhs) const noexcept	{ return value > mRhs.value; }
-				inline bool operator<=(const TDerived& mRhs) const noexcept	{ return value <= mRhs.value; }
-				inline bool operator>=(const TDerived& mRhs) const noexcept	{ return value >= mRhs.value; }
-		};
-	}
-
-	template<typename T, typename TDeleter> class MRItrIdx final : public Internal::MRItrBase<T, TDeleter, std::size_t, MRItrIdx<T, TDeleter>>
-	{
-		template<typename, typename, typename, typename> friend class Internal::MRItrBase;
-
-		private:
-			MemoryManager2<T, TDeleter>* mr;
-			template<typename TT> inline TT getImpl() noexcept { return mr->c[this->value]; }
-
-		public:
-			inline MRItrIdx(MemoryManager2<T, TDeleter>& mMR, std::size_t mIdx) noexcept
-				: Internal::MRItrBase<T, TDeleter, std::size_t, MRItrIdx<T, TDeleter>>{mIdx}, mr(&mMR) { }
-	};
-
-	namespace Internal
-	{
-		template<typename TDerived, typename TItem, typename TDeleter = std::default_delete<TItem>> class MemoryManager2Base
-		{
-			template<typename T, typename TP> friend void ssvu::eraseRemoveIf(T&, const TP&);
-
-			public:
-				VecUptr<TItem, TDeleter> c;
-				std::size_t aliveSize{0u}, tempAlive{0u};
-
-				inline void clear()	noexcept { c.clear(); }
-				inline void del(TItem& mItem) const noexcept { mItem.ssvu_mmAlive = false; }
-
-				// Statically polymorphic methods
-				inline void refresh() { reinterpret_cast<TDerived*>(this)->refreshImpl(); }
-				template<typename TType = TItem, typename... TArgs> inline TType& create(TArgs&&... mArgs)
-				{
-					return reinterpret_cast<TDerived*>(this)->template createTImpl<TType, TArgs...>(std::forward<TArgs>(mArgs)...);
-				}
-
-				template<typename TType> inline static bool isAlive(const TType& mItem) noexcept	{ return mItem->ssvu_mmAlive; }
-				template<typename TType> inline static bool isDead(const TType& mItem) noexcept		{ return !isAlive(mItem); }
-		 };
-	}
-
-	template<typename T, typename TDeleter = std::default_delete<T>> class MemoryManager2 : public Internal::MemoryManager2Base<MemoryManager2<T, TDeleter>, T, TDeleter>
-	{
-		public:
-			inline void refreshImpl()
-			{
-				this->aliveSize = 0u;
-				while(this->aliveSize < this->c.size() && this->template isAlive<Uptr<T, TDeleter>>(this->c[this->aliveSize])) ++this->aliveSize;
-				this->tempAlive = this->aliveSize;
-			}
-			template<typename TType = T, typename... TArgs> inline TType& createTImpl(TArgs&&... mArgs)
-			{
-				auto uptr(makeUptr<TType>(std::forward<TArgs>(mArgs)...));
-				auto result(uptr.get());
-
-				if(this->tempAlive > this->c.size()) this->c[this->tempAlive++] = std::move(uptr);
-				else this->c.emplace_back(std::move(uptr));
-
-				return *result;
-			}
-
-			inline MRItrIdx<T, TDeleter> begin() noexcept { return {*this, 0}; }
-			inline MRItrIdx<T, TDeleter> end() noexcept { return {*this, this->aliveSize}; }
-
-			inline MRItrIdx<T, TDeleter> begin() const noexcept { return {*this, 0}; }
-			inline MRItrIdx<T, TDeleter> end() const noexcept { return {*this, this->aliveSize}; }
-	};
-}
-
-#endif
-
-// Copyright (c) 2013-2014 Vittorio Romeo
-// License: Academic Free License ("AFL") v. 3.0
-// AFL License page: http://opensource.org/licenses/AFL-3.0
-
 #ifndef SSVU_MEMORYMANAGER2REC
 #define SSVU_MEMORYMANAGER2REC
 
@@ -190,54 +72,64 @@ namespace ssvu
 {
 	template<typename> class MemoryManager2Rec;
 
-	namespace Internal
+	template<typename T, typename TMR> class MRItrIdxRec
 	{
-		template<typename T, typename TItrValue, typename TDerived> class MRItrBaseRec
-		{
-			public:
-				using TUptr = UptrRec<T>;
-
-			protected:
-				TItrValue value;
-
-				inline TDerived& getThisDerived() noexcept { return *reinterpret_cast<TDerived*>(this); }
-				template<typename TT> inline TT getImpl() noexcept { return getThisDerived().template getImpl<TT>(); }
-
-			public:
-				inline MRItrBaseRec(TItrValue mValue) noexcept : value{mValue} { }
-
-				inline TDerived& operator++() noexcept				{ ++value;			return getThisDerived(); }
-				inline TDerived& operator++(int) noexcept			{ ++value;			return getThisDerived(); }
-				inline TDerived& operator--() noexcept				{ --value;			return getThisDerived(); }
-				inline TDerived& operator--(int) noexcept			{ --value;			return getThisDerived(); }
-				inline TDerived& operator+=(int mOffset) noexcept	{ value += mOffset;	return getThisDerived(); }
-				inline TDerived& operator-=(int mOffset) noexcept	{ value -= mOffset;	return getThisDerived(); }
-
-				inline TUptr& operator*() noexcept				{ return getImpl<TUptr&>(); }
-				inline const TUptr& operator*() const noexcept	{ return getImpl<const TUptr&>(); }
-				inline TUptr* operator->() noexcept				{ return &(getImpl<TUptr&>()); }
-				inline const TUptr* operator->() const noexcept	{ return &(getImpl<const TUptr&>()); }
-
-				inline bool operator==(const TDerived& mRhs) const noexcept	{ return value == mRhs.value; }
-				inline bool operator!=(const TDerived& mRhs) const noexcept	{ return value != mRhs.value; }
-				inline bool operator<(const TDerived& mRhs) const noexcept	{ return value < mRhs.value; }
-				inline bool operator>(const TDerived& mRhs) const noexcept	{ return value > mRhs.value; }
-				inline bool operator<=(const TDerived& mRhs) const noexcept	{ return value <= mRhs.value; }
-				inline bool operator>=(const TDerived& mRhs) const noexcept	{ return value >= mRhs.value; }
-		};
-	}
-
-	template<typename T> class MRItrIdxRec final : public Internal::MRItrBaseRec<T, std::size_t, MRItrIdxRec<T>>
-	{
-		template<typename, typename, typename> friend class Internal::MRItrBaseRec;
+		public:
+			using TUptr = UptrRec<T>;
 
 		private:
-			MemoryManager2Rec<T>* mr;
-			template<typename TT> inline TT getImpl() noexcept { return mr->c[this->value]; }
+			TMR& mr;
+			std::size_t value;
 
 		public:
-			inline MRItrIdxRec(MemoryManager2Rec<T>& mMR, std::size_t mIdx) noexcept
-				: Internal::MRItrBaseRec<T, std::size_t, MRItrIdxRec<T>>{mIdx}, mr(&mMR) { }
+			inline MRItrIdxRec(TMR& mMR, std::size_t mIdx) noexcept : mr(mMR), value(mIdx) { }
+
+			inline MRItrIdxRec& operator++() noexcept				{ ++value;			return *this; }
+			inline MRItrIdxRec& operator++(int) noexcept			{ ++value;			return *this; }
+			inline MRItrIdxRec& operator--() noexcept				{ --value;			return *this; }
+			inline MRItrIdxRec& operator--(int) noexcept			{ --value;			return *this; }
+			inline MRItrIdxRec& operator+=(int mOffset) noexcept	{ value += mOffset;	return *this; }
+			inline MRItrIdxRec& operator-=(int mOffset) noexcept	{ value -= mOffset;	return *this; }
+
+			inline TUptr& operator*() noexcept				{ return mr.c[this->value]; }
+			inline TUptr* operator->() noexcept				{ return &(mr.c[this->value]); }
+
+			inline bool operator==(const MRItrIdxRec& mRhs) const noexcept	{ return value == mRhs.value; }
+			inline bool operator!=(const MRItrIdxRec& mRhs) const noexcept	{ return value != mRhs.value; }
+			inline bool operator<(const MRItrIdxRec& mRhs) const noexcept	{ return value < mRhs.value; }
+			inline bool operator>(const MRItrIdxRec& mRhs) const noexcept	{ return value > mRhs.value; }
+			inline bool operator<=(const MRItrIdxRec& mRhs) const noexcept	{ return value <= mRhs.value; }
+			inline bool operator>=(const MRItrIdxRec& mRhs) const noexcept	{ return value >= mRhs.value; }
+	};
+
+	template<typename T, typename TMR> class MRItrIdxRecC
+	{
+		public:
+			using TUptr = UptrRec<T>;
+
+		private:
+			TMR& mr;
+			std::size_t value;
+
+		public:
+			inline MRItrIdxRecC(TMR& mMR, std::size_t mIdx) noexcept : mr(mMR), value(mIdx) { }
+
+			inline MRItrIdxRecC& operator++() noexcept				{ ++value;			return *this; }
+			inline MRItrIdxRecC& operator++(int) noexcept			{ ++value;			return *this; }
+			inline MRItrIdxRecC& operator--() noexcept				{ --value;			return *this; }
+			inline MRItrIdxRecC& operator--(int) noexcept			{ --value;			return *this; }
+			inline MRItrIdxRecC& operator+=(int mOffset) noexcept	{ value += mOffset;	return *this; }
+			inline MRItrIdxRecC& operator-=(int mOffset) noexcept	{ value -= mOffset;	return *this; }
+
+			inline const TUptr& operator*() const noexcept	{ return mr.c.at(this->value); }
+			inline const TUptr* operator->() const noexcept	{ return &(mr.c.at(this->value)); }
+
+			inline bool operator==(const MRItrIdxRecC& mRhs) const noexcept	{ return value == mRhs.value; }
+			inline bool operator!=(const MRItrIdxRecC& mRhs) const noexcept	{ return value != mRhs.value; }
+			inline bool operator<(const MRItrIdxRecC& mRhs) const noexcept	{ return value < mRhs.value; }
+			inline bool operator>(const MRItrIdxRecC& mRhs) const noexcept	{ return value > mRhs.value; }
+			inline bool operator<=(const MRItrIdxRecC& mRhs) const noexcept	{ return value <= mRhs.value; }
+			inline bool operator>=(const MRItrIdxRecC& mRhs) const noexcept	{ return value >= mRhs.value; }
 	};
 
 	namespace Internal
@@ -270,26 +162,33 @@ namespace ssvu
 		public:
 			inline void refreshImpl()
 			{
-				this->aliveSize = 0u;
-				while(this->aliveSize < this->c.size() && this->template isAlive<UptrRec<T>>(this->c[this->aliveSize])) ++this->aliveSize;
-				this->tempAlive = this->aliveSize;
+				//this->aliveSize = 0u;
+				//while(this->aliveSize < this->c.size() && this->template isAlive<UptrRec<T>>(this->c[this->aliveSize])) ++this->aliveSize;
+				//this->tempAlive = this->aliveSize;
+
+				eraseRemoveIf(this->c, this->template isDead<UptrRec<T>>);
+				this->aliveSize = this->c.size();
 			}
 			template<typename TType = T, typename... TArgs> inline TType& createTImpl(TArgs&&... mArgs)
 			{
 				auto uptr(makeUptrRecPoly<TType, T>(std::forward<TArgs>(mArgs)...));
 				auto result(uptr.get());
 
-				if(this->tempAlive > this->c.size()) this->c[this->tempAlive++] = std::move(uptr);
-				else this->c.emplace_back(std::move(uptr));
+				//if(this->tempAlive > this->c.size())
+				//	this->c[this->tempAlive++] = std::move(uptr);
+				//else
+					this->c.emplace_back(std::move(uptr));
 
 				return *result;
 			}
 
-			inline MRItrIdxRec<T> begin() noexcept { return {*this, 0}; }
-			inline MRItrIdxRec<T> end() noexcept { return {*this, this->aliveSize}; }
+			inline std::size_t size() const noexcept { return this->c.size(); }
 
-			inline MRItrIdxRec<T> begin() const noexcept { return {*this, 0}; }
-			inline MRItrIdxRec<T> end() const noexcept { return {*this, this->aliveSize}; }
+			inline MRItrIdxRec<T, MemoryManager2Rec<T>> begin() noexcept { return {*this, 0}; }
+			inline MRItrIdxRec<T, MemoryManager2Rec<T>> end() noexcept { return {*this, this->aliveSize}; }
+
+			inline const MRItrIdxRecC<T, const MemoryManager2Rec<T>> begin() const noexcept { return MRItrIdxRecC<T, const MemoryManager2Rec<T>>{*this, 0}; }
+			inline const MRItrIdxRecC<T, const MemoryManager2Rec<T>> end() const noexcept { return MRItrIdxRecC<T, const MemoryManager2Rec<T>>{*this, this->aliveSize}; }
 	};
 }
 
