5990848253b6c6d392c9e46349a30ae08dd358a3
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-09-26 19:20:04 +0200

    Timeline refactoring

--- include/SSVUtils/Timeline/Timeline.hpp
+++ include/SSVUtils/Timeline/Timeline.hpp
@@ -24,42 +24,48 @@ namespace ssvu
 		template<bool> friend class Internal::WaitLoop;
 		template<typename> friend class Internal::GoImpl;
 
+		public:
+			using Idx = int;
+			static constexpr Idx nullIdx{-1};
+
 		private:
 			PolyRecycler<Command> recycler;
 			std::vector<decltype(recycler)::PtrType> commands;
-			int cIdx{-1};
+			Idx cIdx{nullIdx};
 			FT remainder{0.f};
 			bool ready{true}, finished{false};
 
-			template<typename T, typename... TArgs> inline int insertImpl(std::size_t mIdx, TArgs&&... mArgs)
+			inline bool isCIdxNull() const noexcept { return cIdx == nullIdx; }
+
+			template<typename T, typename... TArgs> inline Idx insertImpl(Idx mIdx, TArgs&&... mArgs)
 			{
 				commands.emplace(std::begin(commands) + mIdx, recycler.create<T>(*this, fwd<TArgs>(mArgs)...));
-				if(cIdx == -1) cIdx = mIdx;
+
+				// If the current command is null, set it to the newly added one
+				if(isCIdxNull()) cIdx = mIdx;
 
 				return mIdx;
 			}
 
 			inline void next()
 			{
-				if(cIdx == -1) return;
-				auto itr(std::begin(commands) + cIdx);
+				if(isCIdxNull()) return;
 
-				if(itr == std::end(commands) - 1) { cIdx = -1; return; } // no more commands, return
-				if(itr < std::end(commands) - 1 && itr >= std::begin(commands)) ++cIdx;
+				++cIdx;
+				auto itr(std::begin(commands) + cIdx);
+				if(itr == std::end(commands)) cIdx = nullIdx;
 			}
 
 		public:
 			inline Timeline(bool mStart = true) noexcept { if(!mStart) stop(); }
 
-			template<typename T, typename... TArgs> inline auto append(TArgs&&... mArgs)					{ return insertImpl<T>(commands.size(), fwd<TArgs>(mArgs)...); }
-			template<typename T, typename... TArgs> inline auto insert(std::size_t mIdx, TArgs&&... mArgs)	{ return insertImpl<T>(mIdx, fwd<TArgs>(mArgs)...); }
+			template<typename T, typename... TArgs> inline auto append(TArgs&&... mArgs)			{ return insertImpl<T>(commands.size(), fwd<TArgs>(mArgs)...); }
+			template<typename T, typename... TArgs> inline auto insert(Idx mIdx, TArgs&&... mArgs)	{ return insertImpl<T>(mIdx, fwd<TArgs>(mArgs)...); }
 
-			//inline void del(Command& mCommand)				{ eraseRemoveIf(commands, [&mCommand](const auto& mC){ mC.get() == &mCommand; }); }
-			inline void jumpTo(std::size_t mIdx) noexcept	{ cIdx = mIdx; }
-			//inline void jumpTo(Command& mCommand) noexcept	{ cIdx = &mCommand; }
-			inline void start()	noexcept					{ finished = false; ready = true; }
-			inline void clear()	noexcept					{ cIdx = -1; commands.clear(); finished = true; }
-			inline void stop() noexcept						{ finished = true; ready = false; }
+			inline void jumpTo(Idx mIdx) noexcept	{ cIdx = mIdx; }
+			inline void start()	noexcept			{ finished = false; ready = true; }
+			inline void clear()	noexcept			{ commands.clear(); cIdx = nullIdx; finished = true; }
+			inline void stop() noexcept				{ finished = true; ready = false; }
 
 			inline void update(FT mFT)
 			{
@@ -68,24 +74,24 @@ namespace ssvu
 
 				do
 				{
-					if(cIdx == -1) { stop(); break; }
+					if(isCIdxNull()) { stop(); break; }
 					commands[cIdx]->update(mFT + remainder);
-					remainder = 0;
+					remainder = 0.f;
 				} while(ready);
 			}
 			inline void reset()
 			{
 				start();
 				for(const auto& c : commands) c->reset();
-				cIdx = commands.empty() ? -1 : 0;
+				cIdx = commands.empty() ? nullIdx : 0;
 			}
 
 			inline auto getSize() const noexcept	{ return commands.size(); }
 			inline bool isFinished() const noexcept	{ return finished; }
 			inline int getCurrentIndex() const
 			{
-				if(cIdx == -1) return 0;
-				return cIdx < static_cast<decltype(cIdx)>(commands.size()) ? cIdx : -1;
+				if(isCIdxNull()) return 0;
+				return cIdx < static_cast<Idx>(commands.size()) ? cIdx : nullIdx;
 			}
 	};
 }
@@ -96,6 +102,3 @@ namespace ssvu
 #include "SSVUtils/Timeline/TimelineManager.hpp"
 
 #endif
-
-// TODO: store index instead of pointer for current command?
-// TODO: use linked list! (maybe intrusive?)
