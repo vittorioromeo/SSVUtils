66bc171f06cf720491599917073970b7819d4a1b
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-09 16:42:31 +0200

    Moved typedefs in Common.hpp

--- include/SSVUtils/Json/Num/Num.hpp
+++ include/SSVUtils/Json/Num/Num.hpp
@@ -20,10 +20,6 @@ namespace ssvu
 				public:
 					enum class Type{IntS, IntU, Real};
 
-					using IntS = long int;
-					using IntU = unsigned long int;
-					using Real = double;
-
 				private:
 					Type type;
 
@@ -77,7 +73,6 @@ namespace ssvu
 
 					template<typename T> void set(const T& mX) noexcept	{ Internal::NumHelper<T>::set(*this, mX); }
 					template<typename T> auto as() const noexcept		{ return Internal::NumHelper<T>::as(*this); }
-					// TODO: isStoredAs<unsigned int>()... == IntU
 
 					inline auto getType() const noexcept { return type; }
 
