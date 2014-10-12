d0366aa8e61fa7ad8438451054dd5435c5dae016
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-12 22:22:42 +0200

    std::size_t -> SizeT

--- include/SSVUtils/TemplateSystem/Internal/Expander.hpp
+++ include/SSVUtils/TemplateSystem/Internal/Expander.hpp
@@ -26,19 +26,19 @@ namespace ssvu
 				const std::string& src;
 				std::string& bufResult;
 				std::string& bufKey;
-				const std::size_t idxBegin, idxEnd;
-				std::size_t idx, sectIdxCntStart, sectIdxCntEnd, sectIdxStart, sectIdxEnd;
+				const SizeT idxBegin, idxEnd;
+				SizeT idx, sectIdxCntStart, sectIdxCntEnd, sectIdxStart, sectIdxEnd;
 				Settings settings;
 				bool separate;
 
-				inline auto getC(std::size_t mIdx) const noexcept 	{ SSVU_ASSERT(mIdx >= 0 && mIdx < src.size()); return src[mIdx]; }
-				inline auto getC() const noexcept 					{ return getC(idx); }
+				inline auto getC(SizeT mIdx) const noexcept	{ SSVU_ASSERT(mIdx >= 0 && mIdx < src.size()); return src[mIdx]; }
+				inline auto getC() const noexcept 			{ return getC(idx); }
 
 				bool replace();
 				bool replaceSection();
 
 			public:
-				inline Expander(const Dictionary& mDict, const std::string& mSrc, std::string& mBufResult, std::string& mBufKey, std::size_t mIdxBegin, std::size_t mIdxEnd, bool mSeparate, Settings mSettings)
+				inline Expander(const Dictionary& mDict, const std::string& mSrc, std::string& mBufResult, std::string& mBufKey, SizeT mIdxBegin, SizeT mIdxEnd, bool mSeparate, Settings mSettings)
 					: dict{mDict}, src{mSrc}, bufResult{mBufResult}, bufKey{mBufKey}, idxBegin{mIdxBegin}, idxEnd{mIdxEnd}, idx{idxBegin}, separate{mSeparate}, settings{mSettings} { }
 
 				bool expand();
