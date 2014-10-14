94a8adc8726192a44845398d9ef4aafdc1e2affb
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-14 15:26:29 +0200

    docs

--- include/SSVUtils/Benchmark/Benchmark.hpp
+++ include/SSVUtils/Benchmark/Benchmark.hpp
@@ -110,13 +110,13 @@ namespace ssvu
 
 /// @macro Instantiates a `LogScopeExit` temp-named object in the current scope.
 /// @details The istantiated object will run a benchmark during its lifetime.
-/// Pass the desired benchmark title as a parameter.
+/// Pass the desired benchmark title as a parameter. Requires semicolon.
 #define SSVU_BENCHMARK_LOG_SCOPE_EXIT(...) \
 	::ssvu::Benchmark::Internal::LogScopeExit SSVPP_CAT(__logScopeExit, __LINE__){__VA_ARGS__}
 
 /// @macro Instantiates a `RunGroupScopeExit` temp-named object in the current scope.
 /// @details The istantiated object will resume and pause a group benchmark during its lifetime.
-/// Pass the desired benchmark group as a parameter.
+/// Pass the desired benchmark group as a parameter. Requires semicolon.
 #define SSVU_BENCHMARK_RUN_GROUP_SCOPE_EXIT(...) \
 	::ssvu::Benchmark::Internal::RunGroupScopeExit SSVPP_CAT(__logGroupScopeExit, __LINE__){__VA_ARGS__}
 
