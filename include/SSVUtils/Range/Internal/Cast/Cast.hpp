7da90a22ee5204a936ff71d389260cc54ba2a7e7
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-14 15:27:38 +0200

    name refactoring

--- include/SSVUtils/Range/Internal/Cast/Cast.hpp
+++ include/SSVUtils/Range/Internal/Cast/Cast.hpp
@@ -9,36 +9,36 @@ namespace ssvu
 {
 	namespace Internal
 	{
-		/// @brief Cast iterator adaptor implementation.
-		template<typename T> struct AdaptorImplCast
+		/// @brief Cast ptr iterator adaptor implementation.
+		template<typename T> struct AdaptorImplCastPtr
 		{
 			template<typename TItr> inline static T get(TItr mItr) noexcept { return reinterpret_cast<T>(**mItr);  }
 		};
 
-		/// @typedef Typedef for a cast iterator.
-		template<typename T, typename TItr> using ItrCast = AdaptorFromItr<TItr, AdaptorImplCast<T>>;
+		/// @typedef Typedef for a cast ptr iterator.
+		template<typename T, typename TItr> using ItrCastPtr = AdaptorFromItr<TItr, AdaptorImplCastPtr<T>>;
 
-		/// @brief Creates and returns a cast iterator.
+		/// @brief Creates and returns a cast ptr iterator.
 		/// @details The cast adaptor automatically dereferences pointer-like objects and casts them to `T`.
-		template<typename T, typename TItr> inline auto makeItrCast(TItr mItr) noexcept { return ItrCast<T, TItr>{mItr}; }
+		template<typename T, typename TItr> inline auto makeItrCastPtr(TItr mItr) noexcept { return ItrCastPtr<T, TItr>{mItr}; }
 
-		/// @brief Creates and returns a const cast iterator.
+		/// @brief Creates and returns a const cast ptr iterator.
 		/// @details The cast adaptor automatically dereferences pointer-like objects and casts them to `const T`.
-		template<typename T, typename TItr> inline auto makeItrCastConst(TItr mItr) noexcept { return ItrCast<const T, TItr>{mItr}; }
+		template<typename T, typename TItr> inline auto makeItrCastPtrConst(TItr mItr) noexcept { return ItrCastPtr<const T, TItr>{mItr}; }
 	}
 
 	/// @brief Creates and returns a cast iterator range.
 	/// @details The cast range automatically dereferences pointer-like objects and casts them to `T`.
-	template<typename T, typename TC> inline auto asRangeCast(TC& mContainer) noexcept
+	template<typename T, typename TC> inline auto asRangeCastPtr(TC& mContainer) noexcept
 	{
-		return makeRange(Internal::makeItrCast<T>(std::begin(mContainer)), Internal::makeItrCast<T>(std::end(mContainer)));
+		return makeRange(Internal::makeItrCastPtr<T>(std::begin(mContainer)), Internal::makeItrCastPtr<T>(std::end(mContainer)));
 	}
 
 	/// @brief Creates and returns a const cast iterator range.
 	/// @details The cast range automatically dereferences pointer-like objects and casts them to `const T`.
-	template<typename T, typename TC> inline auto asRangeCast(const TC& mContainer) noexcept
+	template<typename T, typename TC> inline auto asRangeCastPtr(const TC& mContainer) noexcept
 	{
-		return makeRange(Internal::makeItrCastConst<T>(std::cbegin(mContainer)), Internal::makeItrCastConst<T>(std::cend(mContainer)));
+		return makeRange(Internal::makeItrCastPtrConst<T>(std::cbegin(mContainer)), Internal::makeItrCastPtrConst<T>(std::cend(mContainer)));
 	}
 }
 
