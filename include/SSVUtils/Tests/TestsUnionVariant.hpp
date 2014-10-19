f173f78743ea0451bb1e9ce117f08ac6cf6e9e69
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-19 18:54:07 +0200

    Tests, todo

--- include/SSVUtils/Tests/TestsUnionVariant.hpp
+++ include/SSVUtils/Tests/TestsUnionVariant.hpp
@@ -5,9 +5,47 @@
 #ifndef SSVU_TESTS_TESTSUNIONVARIANT
 #define SSVU_TESTS_TESTSUNIONVARIANT
 
+#define EXEC_TEST(mType, mX) \
+	{ \
+		u.init<mType>(mX); \
+		SSVUT_EXPECT_OP(u.get<mType>(), ==, mX); \
+		u.deinit<mType>(); \
+	}
+
+#define EXEC_TEST_POD(mType, mX) \
+	{ \
+		u.init<mType>(mX); \
+		SSVUT_EXPECT_OP(u.get<mType>(), ==, mX); \
+	}
+
+
 SSVUT_TEST(UnionVariantTests)
 {
-	// TODO: force debug isclean tests both for pod n stuff
+	using namespace ssvu;
+
+	{
+		UnionVariant<char, short int, int, float, long int, double, std::string> u;
+		EXEC_TEST(char, 'c')
+		EXEC_TEST(short int, 152)
+		EXEC_TEST(int, 152)
+		EXEC_TEST(float, 152.5f)
+		EXEC_TEST(long int, 152)
+		EXEC_TEST(double, 33.21)
+		EXEC_TEST(std::string, "hi")
+	}
+
+	{
+		UnionVariantPOD<char, short int, int, float, long int, double> u;
+		EXEC_TEST_POD(char, 'c')
+		EXEC_TEST_POD(short int, 152)
+		EXEC_TEST_POD(int, 152)
+		EXEC_TEST_POD(float, 152.5f)
+		EXEC_TEST_POD(long int, 152)
+		EXEC_TEST_POD(double, 33.21)
+	}
 }
+
+#undef EXEC_TEST
+#undef EXEC_TEST_POD
 
 #endif
--- include/SSVUtils/UnionVariant/UnionVariant.hpp
+++ include/SSVUtils/UnionVariant/UnionVariant.hpp
@@ -83,4 +83,4 @@ namespace ssvu
 
 #endif
 
-// TODO: create tests, docs
+// TODO: docs
