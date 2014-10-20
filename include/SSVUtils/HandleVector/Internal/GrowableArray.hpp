0d6335e7975983f3acd812d806dd02cc3decad17
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-20 16:43:58 +0200

    Docs

--- include/SSVUtils/HandleVector/Internal/GrowableArray.hpp
+++ include/SSVUtils/HandleVector/Internal/GrowableArray.hpp
@@ -9,12 +9,18 @@ namespace ssvu
 {
 	namespace Internal
 	{
+		/// @brief Low-level growable array storage class.
+		/// @details Data must be explicitly constructed and destroyed.
 		template<typename T> class GrowableArray
 		{
 			private:
+				/// @typedef Aligned storage type for `T`.
 				using TStorage = AlignedStorageFor<T>;
+
+				/// @brief Internal storage.
 				TStorage* data{nullptr};
 
+				/// @brief Implementation of move operations.
 				inline void moveImpl(GrowableArray&& mGA) noexcept
 				{
 					mGA.data = data;
@@ -31,6 +37,8 @@ namespace ssvu
 				inline auto& operator=(const GrowableArray& mGA) = delete;
 				inline auto& operator=(GrowableArray&& mGA) noexcept { moveImpl(mGA); return *this; }
 
+				/// @brief Grows the internal storage from `mCapacityOld` to `mCapacityNew`.
+				/// @details The new capacity must be greater or equal than the old one.
 				inline void grow(SizeT mCapacityOld, SizeT mCapacityNew)
 				{
 					SSVU_ASSERT(mCapacityOld <= mCapacityNew);
@@ -42,17 +50,20 @@ namespace ssvu
 					delete[] newData;
 				}
 
+				/// @brief Constructs a `T` instance at index `mI`.
 				template<typename... TArgs> inline void initAt(SizeT mI, TArgs&&... mArgs)
 					noexcept(isNothrowCtor<T, TArgs...>())
 				{
 					new(&data[mI]) T(fwd<TArgs>(mArgs)...);
 				}
 
+				/// @brief Destroyes a `T` instance at index `mI`.
 				inline void deinitAt(SizeT mI) noexcept(isNothrowDtor<T>())
 				{
 					(*this)[mI].~T();
 				}
 
+				// Getters
 				inline T& operator[](SizeT mI) noexcept				{ return castStorage<T>(data[mI]); }
 				inline const T& operator[](SizeT mI) const noexcept	{ return castStorage<T>(data[mI]); }
 		};
@@ -60,5 +71,3 @@ namespace ssvu
 }
 
 #endif
-
-// TODO: docs
