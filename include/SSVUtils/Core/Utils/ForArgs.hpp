9c8c4eb4ffa9a484f2a7bf9a8f7f9f684d2508dc
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-02-01 18:04:14 +0100

    for args improvements

--- include/SSVUtils/Core/Utils/ForArgs.hpp
+++ include/SSVUtils/Core/Utils/ForArgs.hpp
@@ -8,9 +8,9 @@
 namespace ssvu
 {
 	/// @brief Calls `mFn` for each argument passed to the function.
-	template<typename TF, typename... TArgs> inline TF forArgs(TF&& mFn, TArgs&&... mArgs)
+	template<typename TF, typename... TArgs> inline auto forArgs(TF&& mFn, TArgs&&... mArgs)
 	{
-		return (void) std::initializer_list<int>{(std::ref(mFn)((TArgs&&) mArgs), 0)...}, SSVU_FWD(mFn);
+		return (void) std::initializer_list<int>{0, (std::ref(mFn)(SSVU_FWD(mArgs)), 0)...}, SSVU_FWD(mFn);
 	}
 }
 
