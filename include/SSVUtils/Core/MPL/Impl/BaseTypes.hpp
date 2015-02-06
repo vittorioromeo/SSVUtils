f50fd9b4902e3c3ff491981f04976b6df78dd2eb
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-02-06 12:55:28 +0100

    fix min

--- include/SSVUtils/Core/MPL/Impl/BaseTypes.hpp
+++ include/SSVUtils/Core/MPL/Impl/BaseTypes.hpp
@@ -18,19 +18,19 @@ namespace ssvu
 
 		template<typename T> inline constexpr T getMin(T mA) noexcept { return mA; }
 		template<typename T> inline constexpr T getMin(T mA, T mB) noexcept { return mA < mB ? mA : mB; }
-		template<typename T, typename... TArgs> inline constexpr T getMin(T mA, T mB, TArgs... mX) noexcept { return getMin(mA, getMin(mB, mX...)); }
+		template<typename T, typename... TArgs> inline constexpr T getMin(T mA, T mB, TArgs... mX) noexcept { return getMin<T>(mA, getMin<T>(mB, mX...)); }
 
 		template<typename T> inline constexpr T getMax(T mA) noexcept { return mA; }
 		template<typename T> inline constexpr T getMax(T mA, T mB) noexcept { return mA > mB ? mA : mB; }
-		template<typename T, typename... TArgs> inline constexpr T getMax(T mA, T mB, TArgs... mX) noexcept { return getMax(mA, getMax(mB, mX...)); }
+		template<typename T, typename... TArgs> inline constexpr T getMax(T mA, T mB, TArgs... mX) noexcept { return getMax<T>(mA, getMax<T>(mB, mX...)); }
 
 		template<typename T, T TN0> inline constexpr T getMin() noexcept { return TN0; }
 		template<typename T, T TN0, T TN1> inline constexpr T getMin() noexcept { return TN0 < TN1 ? TN0 : TN1; }
-		template<typename T, T TN0, T TN1, T... TArgs> inline constexpr T getMin() noexcept { return getMin<TN0, getMin<TN1, TArgs...>()>(); }
+		template<typename T, T TN0, T TN1, T... TArgs> inline constexpr T getMin() noexcept { return getMin<T, TN0, getMin<T, TN1, TArgs...>()>(); }
 
 		template<typename T, T TN0> inline constexpr T getMax() noexcept { return TN0; }
 		template<typename T, T TN0, T TN1> inline constexpr T getMax() noexcept { return TN0 > TN1 ? TN0 : TN1; }
-		template<typename T, T TN0, T TN1, T... TArgs> inline constexpr T getMax() noexcept { return getMax<TN0, getMax<TN1, TArgs...>()>(); }
+		template<typename T, T TN0, T TN1, T... TArgs> inline constexpr T getMax() noexcept { return getMax<T, TN0, getMax<T, TN1, TArgs...>()>(); }
 	}
 }
 
