890a32b9639b71b63e3c29c06b981498c347744d
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-02-19 18:52:56 +0100

    license

--- include/SSVUtils/Easing/Easing.hpp
+++ include/SSVUtils/Easing/Easing.hpp
@@ -1,4 +1,4 @@
-// Copyright (mC) 2013-2014 Vittorio Romeo
+// Copyright (mC) 2013-2015 Vittorio Romeo
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 // Adapted from: http://www.robertpenner.com/easing/
