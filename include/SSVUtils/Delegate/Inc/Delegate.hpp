6af6c20ee14caf95eaea4a07698231fbec66635f
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-02-19 18:37:26 +0100

    copyright

--- include/SSVUtils/Delegate/Inc/Delegate.hpp
+++ include/SSVUtils/Delegate/Inc/Delegate.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2014 Vittorio Romeo
+// Copyright (c) 2013-2015 Vittorio Romeo
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 // Thanks to sim642 - http://www.facepunch.com/showthread.php?t=1239244&p=39204496&viewfull=1#post39204496
